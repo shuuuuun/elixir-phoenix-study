4ce341bb5aa7a9d776705738b8448f51ab0bba45
motoki-shun <motoki-shun@kayac.com>
2019-08-31 16:25:35 +0900

    using socket.

--- hello/lib/hello_web/channels/user_socket.ex
+++ hello/lib/hello_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule HelloWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", HelloWeb.RoomChannel
+  channel "room:*", HelloWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
